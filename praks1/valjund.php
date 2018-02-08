4eee92fdd42e0b4d450b78cbffe2a79a48842846
jaanika.muursepp <jaanika.muursepp@khk.ee>
2018-02-08 12:34:43 +0200

    Defineeriti erinevate väärtustega muutujad

--- praks1/valjund.php
+++ praks1/valjund.php
@@ -1,6 +1,22 @@
-  * realine
-  * kommentaar
-  */
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jaanikamy
+ * Date: 08.02.2018
+ * Time: 12:32
+ */
+// üherealine kommenraar
+/*
+* mitme
+* realine
+* kommentaar
+*/
 // väljastamine
 echo 'Aine - Veebiprogrammeerimine'.'<br />'; // käsu lõpp - ;
 print 'valikaine<br />';
+// muutujate defineerimine
+$eesNimi = 'Jaanika'; // eesnimi, string
+$pereNimi = 'Müürisepp'; // perenimi , string
+$bussiNumber = 4; // bussi number, integer
+$soiduKestvus = 0.8; // sõidu kestvus, 0.8 tundi, float
