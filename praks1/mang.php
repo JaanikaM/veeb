ffff8bc9497ace89c285ff7c6f850587b3d45f23
JaanikaM <jaanika.muursepp@khk.ee>
2018-02-14 13:40:39 +0200

    Lisati arvu jälgimine serveris

--- praks1/mang.php
+++ praks1/mang.php
@@ -3,11 +3,17 @@
  * Created by PhpStorm.
  * User: jaanika.muursepp
  * Date: 14.02.2018
- * Time: 13:38
+ * Time: 13:40
  */
 echo '
     <form method="post" action="'.$_SERVER['PHP_SELF'].'">
-        Sisesta arv vahemikus 0-50: <input type="number"><br />
+        Sisesta arv vahemikus 0-50: 
+        <input type="number" name="kasutajaArv"><br />
         <input type="submit" value="Kontrolli">
     </form>
 ';
+// määrame arvu
+$serveriArv = 10;
+$kasutajaArv = $_POST['kasutajaArv'];
+// kontrollime kasutaja väärtus
+echo $kasutajaArv;
