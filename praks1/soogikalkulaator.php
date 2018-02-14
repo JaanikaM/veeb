4f1edf34eed018d85b135a05899d785a1138a1fa
JaanikaM <jaanika.muursepp@khk.ee>
2018-02-14 13:24:26 +0200

    Kontrolliti funktsiooni tööd erinevate soodustuste arvutamiseks

---
+++ praks1/soogikalkulaator.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: jaanika.muursepp
+ * Date: 14.02.2018
+ * Time: 13:23
+ */
+// funktsioon soodustuse arvutamiseks
+/**
+ * @param $taisHind
+ * @param $soodusKaart
+ * @param $kasOledOpilane
+ */
+function soogiHind($taisHind, $soodusKaart, $kasOledOpilane){
+    // funktsiooni sisu
+    $soodusHind = $taisHind;
+    $soodustusProtsent = 15; // %
+    $opilaseToetus = 1.80; // EUR
+    if($soodusKaart){
+        $soodusHind = $taisHind * ((100 - $soodustusProtsent) / 100);
+    }
+    if($kasOledOpilane){
+        $soodusHind = $soodusHind - $opilaseToetus;
+        // $soodusHind -= $opilaseToetus; // - operaatori lühendatud kuju
+    }
+    return $soodusHind;
+}// funktsiooni lõpp
+// kutsume funktsiooni tööle
+// kui oled õpilane
+$soogiHind = soogiHind(2.65, true, true);
+echo 'Prae hind õpilasele = '.round($soogiHind, 2).' €<br />';
+// kui olemas klinedi kaart, aga pole õpilane
+$soogiHind = soogiHind(2.65, true, false);
+echo 'Prae hind sooduskaardi omanikule = '.round($soogiHind, 2).' €<br />';
+// kui ei ole kliendi kaarti
+$soogiHind = soogiHind(2.65, false, false);
+echo 'Prae hind = '.round($soogiHind, 2).' €<br />';
