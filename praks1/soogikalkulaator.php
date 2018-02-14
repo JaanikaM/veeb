e39f7d40f89823da8e8b36428153b5e22833d81e
JaanikaM <jaanika.muursepp@khk.ee>
2018-02-14 13:36:34 +0200

    Lisati arvu arvamise vorm

--- praks1/soogikalkulaator.php
+++ praks1/soogikalkulaator.php
@@ -3,67 +3,11 @@
  * Created by PhpStorm.
  * User: jaanika.muursepp
  * Date: 14.02.2018
- * Time: 13:25
+ * Time: 13:36
  */
-require_once 'funktsioonid.php'; // ligipääs funktsioonid.php failile
-// väljastame html-i vorm
-loeVormFailist('vorm.html');
-var_dump($_POST);
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
-// testimiseks paneme erinevad väärtused paika
-// kasutame selleks massiivi kujul (soodusKaart, kasOledOpilane)
-//
-// kasutajad on tabel (massiiv), kus
-// 1. real on õpilase andmed
-// 2. real on õpetaja andmed
-// 3. real on külalise andmed
-$kasutajad = array(
-    array(
-        'roll' => 'õpilasele',
-        'soodus' => true,
-        'opilaskaart' => true
-    ),
-    array(
-        'roll' => 'õpetajale',
-        'soodus' => true,
-        'opilaskaart' => false
-    ),
-    array(
-        'roll' => 'külalisele',
-        'soodus' => false,
-        'opilaskaart' => false
-    )
-);
-// erinevad söögid
-$praed = array(
-    array(
-        'nimetus' => 'Šnitsel',
-        'kirjeldus' => 'šnitsel sealihast, lisand, kaste, salat, leib',
-        'hind' => 2.68
-    ),
-    array(
-        'nimetus' => 'Seapraad',
-        'kirjeldus' => 'seapraad, lisand, kaste, salat, leib',
-        'hind' => 2.65
-    )
-);
-// vaatame $kasutajad masiivi läbi
-foreach ($praed as $praad){
-    echo '<h1>'.$praad['nimetus'].'</h1>';
-    echo '<code>'.$praad['kirjeldus'].'</code><br />';
-    echo '<ul>';
-    foreach ($kasutajad as $kasutaja) {
-        // täinedame võrdlemisega vormi poolt tulnud andmetega
-        // kui ei ole veel vormi poolt andmed tulnud
-        if(empty($_POST)){
-            $_POST['roll'] = "külalisele";
-        }
-        if ($_POST['roll'] == $kasutaja['roll']) {
-            $soogiHind = soogiHind($praad['hind'], $kasutaja['soodus'], $kasutaja['opilaskaart']);
-            echo '<dd>Prae hind ' . $kasutaja['roll'] . ' = ' . round($soogiHind, 2) . ' €</dd><br />';
-        }
-    }
-    echo '</ul>';
-}
+echo '
+    <form method="post" action="'.$_SERVER['PHP_SELF'].'">
+        Sisesta arv vahemikus 0-50: <input type="number"><br />
+        <input type="submit" value="Kontrolli">
+    </form>
+';
