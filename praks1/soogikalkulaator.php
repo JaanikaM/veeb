c9b174cfc19395ea49ef788906602ae1dc0933bc
JaanikaM <jaanika.muursepp@khk.ee>
2018-02-14 13:33:39 +0200

    Rakendati vormi loomist failist

--- praks1/soogikalkulaator.php
+++ praks1/soogikalkulaator.php
@@ -5,43 +5,54 @@
  * Date: 14.02.2018
  * Time: 13:25
  */
-// funktsioon soodustuse arvutamiseks
-/**
- * @param $taisHind
- * @param $soodusKaart
- * @param $kasOledOpilane
- */
-function soogiHind($taisHind, $soodusKaart = false, $kasOledOpilane = false){
-    // funktsiooni sisu
-    $soodusHind = $taisHind;
-    $soodustusProtsent = 15; // %
-    $opilaseToetus = 1.80; // EUR
-    if($soodusKaart){
-        $soodusHind = $taisHind * ((100 - $soodustusProtsent) / 100);
+require_once 'funktsioonid.php'; // ligipääs funktsioonid.php failile
+// väljastame html-i vorm
+loeVormFailist('vorm.html');
+// testimiseks paneme erinevad väärtused paika
+// kasutame selleks massiivi kujul (soodusKaart, kasOledOpilane)
+//
+// kasutajad on tabel (massiiv), kus
+// 1. real on õpilase andmed
+// 2. real on õpetaja andmed
+// 3. real on külalise andmed
+$kasutajad = array(
+    array(
+        'roll' => 'õpilasele',
+        'soodus' => true,
+        'opilaskaart' => true
+    ),
+    array(
+        'roll' => 'õpetajale',
+        'soodus' => true,
+        'opilaskaart' => false
+    ),
+    array(
+        'roll' => 'külalisele',
+        'soodus' => false,
+        'opilaskaart' => false
+    )
+);
+// erinevad söögid
+$praed = array(
+    array(
+        'nimetus' => 'Šnitsel',
+        'kirjeldus' => 'šnitsel sealihast, lisand, kaste, salat, leib',
+        'hind' => 2.68
+    ),
+    array(
+        'nimetus' => 'Seapraad',
+        'kirjeldus' => 'seapraad, lisand, kaste, salat, leib',
+        'hind' => 2.65
+    )
+);
+// vaatame $kasutajad masiivi läbi
+foreach ($praed as $praad){
+    echo '<h1>'.$praad['nimetus'].'</h1>';
+    echo '<code>'.$praad['kirjeldus'].'</code><br />';
+    echo '<ul>';
+    foreach ($kasutajad as $kasutaja){
+        $soogiHind = soogiHind($praad['hind'], $kasutaja['soodus'], $kasutaja['opilaskaart']);
+        echo '<dd>Prae hind '.$kasutaja['roll'].' = '.round($soogiHind, 2).' €</dd><br />';
     }
-    if($kasOledOpilane){
-        $soodusHind = $soodusHind - $opilaseToetus;
-        // $soodusHind -= $opilaseToetus; // - operaatori lühendatud kuju
-    }
-    return $soodusHind;
-}// funktsiooni lõpp
-// funktsioon vormi väljastamiseks
-// vorm hoiame vorm.html failis
-// vormi sisu loeme antud failist ja
-// väljastame
-function loeVormFailist($failinimi){
-    // siia salvestame sisu
-    $sisu = '';
-    // kontrollime vajaliku faili olemasolu
-    if(file_exists($failinimi) and is_file($failinimi) and is_readable($failinimi)){
-        // saab failist lugeda
-        $fp = fopen($failinimi, 'r'); // tekitame ühendus lugemiseks
-        // loeme failist täissisu
-        $sisu = fread($fp, filesize($failinimi));
-        fclose($fp); // paneme ühendus failiga kinni
-    } else {
-        echo 'Probleem '.$failinimi.' failiga<br />';
-        exit;
-    }
-    echo $sisu;
+    echo '</ul>';
 }
