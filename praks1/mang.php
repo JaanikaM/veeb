019d10c205d365c65f8c21a19a9fcb01519fbbf5
JaanikaM <jaanika.muursepp@khk.ee>
2018-02-21 09:07:52 +0200

    Lisati arvu genereerimine formi andmete abil

--- praks1/mang.php
+++ praks1/mang.php
@@ -7,21 +7,24 @@
  */
 // määrame katsete arvu muutujat
 $katseteArv = $_POST['katseteArv'];
+// määrame serveri arvu muutujat
+$serveriArv = $_POST['serveriArv'];
 // kontrollime antud väärtuse olemasolu
 $katseteArv = isset($katseteArv) ? ++$katseteArv : 0;
+$serveriArv = isset($serveriArv) ? $serveriArv : rand(1, 50);
 // trükime andmetega täidetud vorm
 echo '
     <form method="post" action="'.$_SERVER['PHP_SELF'].'">
         Sisesta arv vahemikus 1-50: 
         <input type="number" name="kasutajaArv"><br />
         <input type="hidden" name="katseteArv" value="'.$katseteArv.'">
+        <input type="hidden" name="serveriArv" value="'.$serveriArv.'">
         <input type="submit" value="Kontrolli">
     </form>
 ';
 // kontrollime, kas arv on edastatud
 if(!empty($_POST['kasutajaArv'])){
     // määrame arvu
-    $serveriArv = 10;
     $kasutajaArv = $_POST['kasutajaArv'];
     if($kasutajaArv > $serveriArv){
         echo 'Pakutud väärtus on suurem<br />';
