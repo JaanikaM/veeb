54e1f2569cd945f4aee17e9b2be2937402064414
JaanikaM <jaanika.muursepp@khk.ee>
2018-02-22 15:26:01 +0200

    Lisati andmebaasi serveriga ühendamise kasutamine

--- praks1/andmetootlus.php
+++ praks1/andmetootlus.php
@@ -5,17 +5,23 @@
  * Date: 22.02.2018
  * Time: 15:22
  */
-require_once 'ab_konf.php';
-// ühendus ab serveriga
-function yhendus(){
-    $ab_yhendus = mysqli_connect(AB_HOST, AB_USER, AB_PASS, AB_NIMI);
-    if($ab_yhendus == false){
-        echo 'Probleem andmebaasi ühendamisega<br />';
-        echo mysqli_connect_error().'<br />';
-        echo mysqli_connect_errno().'<br />';
-        exit;
-    } else {
-        echo 'Ühendus on loodud<br />';
-    }
-    return $ab_yhendus;
-}
+// võtame kasutusele andmebaasitöötlusfunktsioonid
+require_once 'ab_fnk.php';
+echo '<pre>';
+print_r($_POST);
+echo '</pre>';
+// kui on soov kasutada muutujad samade nimedega
+// nagu vormi elementide nimetused
+// siis kasuta
+extract($_POST);
+// kontrollime, mis sai
+echo $eesnimi.'<br />';
+echo $perenimi.'<br />';
+echo $aasta.'<br />';
+echo $kuu.'<br />';
+echo $paev.'<br />';
+// ehitame ajaväärtus DATE formaadi järgi
+$aeg = $aasta.'-'.$kuu.'-'.$paev;
+echo $aeg.'<br />';
+// tekitame ühendus ab serveriga
+$yhendus = yhendus();
