19ff4a69e27468077b72b8b6d64aee98ded5ba1a
JaanikaM <jaanika.muursepp@khk.ee>
2018-02-22 15:26:56 +0200

    Lisati päringu saatmise kirjeldus

--- praks1/andmetootlus.php
+++ praks1/andmetootlus.php
@@ -5,23 +5,29 @@
  * Date: 22.02.2018
  * Time: 15:22
  */
-// võtame kasutusele andmebaasitöötlusfunktsioonid
-require_once 'ab_fnk.php';
-echo '<pre>';
-print_r($_POST);
-echo '</pre>';
-// kui on soov kasutada muutujad samade nimedega
-// nagu vormi elementide nimetused
-// siis kasuta
-extract($_POST);
-// kontrollime, mis sai
-echo $eesnimi.'<br />';
-echo $perenimi.'<br />';
-echo $aasta.'<br />';
-echo $kuu.'<br />';
-echo $paev.'<br />';
-// ehitame ajaväärtus DATE formaadi järgi
-$aeg = $aasta.'-'.$kuu.'-'.$paev;
-echo $aeg.'<br />';
-// tekitame ühendus ab serveriga
-$yhendus = yhendus();
+require_once 'ab_konf.php';
+// ühendus ab serveriga
+function yhendus(){
+    $ab_yhendus = mysqli_connect(AB_HOST, AB_USER, AB_PASS, AB_NIMI);
+    if($ab_yhendus == false){
+        echo 'Probleem andmebaasi ühendamisega<br />';
+        echo mysqli_connect_error().'<br />';
+        echo mysqli_connect_errno().'<br />';
+        exit;
+    } else {
+        echo 'Ühendus on loodud<br />';
+    }
+    return $ab_yhendus;
+}
+// päringute edastamine andmebaasi
+function saadaAndmed($ab_yhendus, $sql){
+    $tulemus = mysqli_query($ab_yhendus, $sql);
+    if($tulemus ==  false){
+        echo 'Probleem päringuga '.$sql.' <br />';
+        echo mysqli_error($ab_yhendus);
+        echo mysqli_errno($ab_yhendus);
+        return false;
+    } else {
+        return $tulemus; // true või andmed
+    }
+}
